0d78b5b9a546329aebfb1b8024d814bd2747f814
Jeff Nguyen <jeff235255@gmail.com>
2017-06-27 11:38:43 +0700

    initialize ionic app by jeff

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,14 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  myVariable: string = "Hello i'm a string, i want to learn Ionic";
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  updateMyvale() {
+    this.myVariable = "Here is the new string after updated.";
   }
 
 }
