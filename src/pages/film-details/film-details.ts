f3a71853381f886d5bbb2af84f72453e5097838e
Jeff Nguyen <jeff235255@gmail.com>
2017-06-30 14:22:18 +0700

    Update api provider

--- src/pages/film-details/film-details.ts
+++ src/pages/film-details/film-details.ts
@@ -1,26 +1,15 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the FilmDetailsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-film-details',
   templateUrl: 'film-details.html',
 })
 export class FilmDetailsPage {
-  filmId = null;
+  film: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.filmId = this.navParams.get('filmId');
+    this.film = this.navParams.get('film');
   }
-
-  goBack() {
-    this.navCtrl.pop();
-  }
-
 }
--- src/pages/films/films.ts
+++ src/pages/films/films.ts
@@ -1,31 +1,27 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { ApiProvider } from './../../providers/api/api';
 
-/**
- * Generated class for the FilmsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
   selector: 'page-films',
   templateUrl: 'films.html',
 })
 export class FilmsPage {
+  films: Observable<any>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public http: Http,
+    public apiProvider: ApiProvider) {
+    this.films = this.apiProvider.getFilms();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad FilmsPage');
-  }
-
-  openDetails() {
-    this.navCtrl.push('FilmDetailsPage', {filmId: 1});
-  }
-
-  goToPlanets() {
-    this.navCtrl.parent.select(2);
+  openDetails(film) {
+    this.navCtrl.push('FilmDetailsPage', {film: film});
   }
 }
